535174b4c3e34b76b9a118148631eb5e4786fb97
Eric Krukowski <erickruk@amazon.com>
2023-07-05 00:50:41 -0400

    Lab complete

--- lib/dog.py
+++ lib/dog.py
@@ -11,5 +11,20 @@ APPROVED_BREEDS = [
     "Pointer"
 ]
 
-class Dog:
-    pass
+class Dog:     
+    def __init__(self, name="Doggo", breed="Mastiff"):
+        self.set_name(name)
+        self.set_breed(breed)
+
+    def get_name(self):
+        return self.name
+    
+    def set_name(self, name):
+        self.name= name.title() if isinstance(name, str) and (0<len(name)<25) else print("Name must be string between 1 and 25 characters.")
+            
+    def get_breed(self):
+        return self.breed
+    
+    def set_breed(self, breed):
+        self.breed = breed if breed in APPROVED_BREEDS else print("Breed must be in list of approved breeds.")
+
--- lib/person.py
+++ lib/person.py
@@ -16,4 +16,21 @@ APPROVED_JOBS = [
 ]
 
 class Person:
-    pass
+    def __init__(self, name="Bob", job="Admin"):
+        self.set_name(name)
+        self.set_job(job)
+
+    def get_name(self):
+        return self.name
+    
+    def set_name(self, name):
+        if isinstance(name, str) and (0<len(name)<25):
+            self.name = ' '.join(name.capitalize() for name in name.split(" "))
+        else: 
+            print("Name must be string between 1 and 25 characters.")
+
+    def get_job(self):
+        return self.job
+
+    def set_job(self, job):
+        self.job = job if job in APPROVED_JOBS else print("Job must be in list of approved jobs.")
